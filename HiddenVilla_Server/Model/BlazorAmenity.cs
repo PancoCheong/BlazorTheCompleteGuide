a48523c0d5ec47be2550a082bb3bb9afcb72e2ae
Panco Cheong <panco.cheong@hotmail.com>
2021-02-15 22:10:02 +0800

    Add project files.

---
+++ HiddenVilla_Server/Model/BlazorAmenity.cs
@@ -0,0 +1,10 @@
+﻿// Model\BlazorAmenitie.cs
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorAmenity
+    {
+        public int Id { get; set; }
+        public string AmenityName { get; set; }
+        public string AmenityDescription { get; set; }
+    }
+}
---
+++ HiddenVilla_Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,10 @@
+﻿// Model\BlazorRoomProp.cs
+namespace HiddenVilla_Server.Model
+{   //Hotel Room Properties, eg. Occupancy, Square Feet, No. of Toilet etc
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
