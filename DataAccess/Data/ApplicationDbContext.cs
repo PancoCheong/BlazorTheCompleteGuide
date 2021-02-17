535fc80e768d35c751135f583fbfe050c7715189
Panco Cheong <panco.cheong@hotmail.com>
2021-02-17 19:05:25 +0800

    Up to 53

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿// DataAccess\Data\ApplicationDbContext.cs
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
